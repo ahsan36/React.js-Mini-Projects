3de67a17dda6e895c9f3f6190c5585825733aa29
Ahsan <ahsaninfo1@gmail.com>
2024-02-09 11:56:26 +0530

    first commit

---
+++ FileSizeValidation/src/App.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react'
+import './App.css'
+
+const App = () => {
+
+  const [selectedFile, setSelectedFile] = useState();
+  const [errorMsg, setErrorMsg] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  // Event handler to update the selected file 
+  const handleFileChange = (event) => {
+    if(event.target.files.length > 0){
+      setSelectedFile(event.target.files[0]);
+    }
+  };
+
+  // Function to validate the selected file size
+  const validateSelectedFile = () => {
+
+    // min and max file size limits (in kilobytes)
+    const MIN_FILE_SIZE = 1024; // 1MB
+    const MAX_FILE_SIZE = 5120; // 5MB
+
+    // Check if a file has been selected
+    if (!selectedFile) {
+      setErrorMsg("Please choose a file");
+      setIsSuccess(false);
+      return;
+    }
+
+    // Calculate the selected file's size in kilobytes
+    const fileSizeKiloBytes = selectedFile.size / 1024;
+
+    // Check if the file size is below the minimum limit
+    if (fileSizeKiloBytes < MIN_FILE_SIZE) {
+      setErrorMsg("File size is less than minimum limit");
+      setIsSuccess(false);
+      return;
+    }
+    
+    // Check if the file size is above the maximum limit
+    if (fileSizeKiloBytes > MAX_FILE_SIZE) {
+      setErrorMsg("File size is greater than maximum limit");
+      setIsSuccess(false);
+      return;
+    }
+
+    // Reset error message and set success status when validation is successful
+    setErrorMsg("");
+    setIsSuccess(true);
+  };
+
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-header">
+          <h4 className="title">File Size Validation</h4>
+        </div>
+
+        <div className="card-body">
+          <p className="label">Choose File</p>
+          
+          {/* Input field for choosing a file */}
+          <input
+            type="file"
+            name='file'
+            onChange={handleFileChange}
+          />
+
+          <div className="info">
+            <p className="info-msg">Min size: 1MB</p>
+            <p className="info-msg">Max size: 5MB</p>
+          </div>
+          
+          {/* Display success message if validation is successful */}
+          {isSuccess ? <p className="success-message">Validation successful</p> : null}
+          
+          {/* Display error message if there is an error */}
+          <p className="err-msg">{errorMsg}</p>
+
+          {/* Button to trigger file size validation */}
+          <button className="btn" onClick={validateSelectedFile}>
+            SUBMIT
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
+

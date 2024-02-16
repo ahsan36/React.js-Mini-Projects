e36aee3f6e93824352580f939fb46b880ac3e997
Ahsan <ahsaninfo1@gmail.com>
2024-02-16 07:45:08 +0530

    first commit

--- CheckBoxList/src/App.jsx
+++ CheckBoxList/src/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
+const LIST_DATA = [
+  { id: "1", value: "Javascript" },
+  { id: "2", value: "Python" },
+  { id: "3", value: "Java" },
+  { id: "4", value: "Kotlin" },
+  { id: "5", value: "Dart" },
+  { id: "6", value: "C#" },
+];
+
 const App = () => {
+
+  const [checkedList, setCheckedList] = useState([]);
+
   return (
-    <div>
-      
+    <div className='container'>
+      <div className="card">
+        <div className="card-header">
+          <p className="title">
+            Select Programming Language
+          </p>
+        </div>
+      </div>      
     </div>
   )
 }

aeb12bb4c83ab0c54f6add0f20c5c07a38fabd41
Ahsan <ahsaninfo1@gmail.com>
2024-02-17 08:00:16 +0530

    first commit

--- CheckBoxList/src/App.jsx
+++ CheckBoxList/src/App.jsx
@@ -14,13 +14,54 @@ const App = () => {
 
   const [checkedList, setCheckedList] = useState([]);
 
+  const handleSelect = (event) => {
+    const value = event.target.value;
+    const isChecked = event.target.checked;
+
+    if(isChecked) {
+      // Add checked item into checkList
+      setCheckedList([...checkedList, value]);
+    }else {
+      // Remove unchecked item from checkList
+      const filteredList = checkedList.filter((item) => item !== value);
+      setCheckedList(filteredList);
+    }
+  }
+
   return (
     <div className='container'>
       <div className="card">
         <div className="card-header">
           <p className="title">
-            Select Skills
+            Please Select Skills
           </p>
+        </div>
+
+        <div className="list-container">
+          <label>You Selected:</label>
+          {checkedList.map((item, index) => {
+            return (
+              <div className="chip">
+                <p className="chip-label">
+                  {item}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="card-body">
+          {LIST_DATA.map((item, index) => {
+            return (
+              <div className="checkbox-container">
+                <input type="checkbox" name="languages" id={item.id} 
+                value={item.value}
+                onChange={handleSelect}
+                />
+                <label htmlFor={item.id}>{item.value}</label>
+              </div>
+            );
+          })}
         </div>
       </div>      
     </div>
